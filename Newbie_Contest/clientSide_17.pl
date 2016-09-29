c3ad1b83ec0f65f682648624e4e1b49d0f5229bd
UJK58M <UJK58M@ADEJEAN.emea.cegedim.grp>
2016-09-29 15:23:27 +0200

    Epreuve 17 ClientSide => Recherche des caractères du code

--- Newbie_Contest/clientSide_17.pl
+++ Newbie_Contest/clientSide_17.pl
@@ -2,6 +2,9 @@
 
 my $res = 225000;
 my $resultat = 0;
+my $nb_lig1 = 0;
+my $nb_lig2 = 0;
+my $nb_lig3 = 0;
 
 print "debut:\n";
 for (my $i=8; $i<20; $i++) {
@@ -32,9 +35,42 @@ for (my $i=8; $i<20; $i++) {
 					#print "j=$j k=$k l=" . ($i-$j-$k) . " => res=$resultat\n";
 					if ( $resultat == $res ) {
 						print "lig3=$j lig2=$k lig1=" . ($i-$j-$k) . "\n";
+						$nb_lig3=$j;
+						$nb_lig2=$k;
+						$nb_lig1=($i-$j-$k);
 					}
 				}
 			}
 		}
 	}
 }
+
+my $res2 = 2607893925;
+my @tab = (2,3,5,7,11,13,17,19,23,29,31,37);
+
+for ( $a=0; $a<10; $a++ ) {
+	for ( $b=$a+1; $b<11; $b++ ) {
+		for ( $c=$b+1; $c<12; $c++ ) {
+			for ( $d=0; $d<10; $d++ ) {
+				for ( $e=$d+1; $e<11; $e++ ) {
+					for ( $f=0; $f<8; $f++ ) {
+						for ( $g=$f+1; $g<9; $g++ ) {
+							for ( $h=$g+1; $h<10; $h++ ) {
+								for ( $i=$h+1; $i<11; $i++ ) {
+									for ( $j=$i+1; $j<12; $j++ ) {
+										$resultat=$tab[$a]*$tab[$b]*$tab[$c]*$tab[$d]*$tab[$e]*$tab[$f]*$tab[$g]*$tab[$h]*$tab[$i]*$tab[$j];
+										#print "$a $b $c $d $e $f $g $h $i $j => $resultat\n";
+										if ( $resultat == $res2 ) {
+											print "$a $b $c $d $e $f $g $h $i $j\n";
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
